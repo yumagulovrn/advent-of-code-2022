c479a9bc2dd2335e7236fd4ca6d31937b3a386c5
Rafael Yumagulov <yumagulovrn@gmail.com>
2022-12-01 22:46:56 +0300

    Day01

---
+++ day01/src/main.rs
@@ -0,0 +1,14 @@
+use itertools::Itertools;
+
+fn main() {
+    let calories = include_str!("../input.txt")
+        .split("\n\n")
+        .map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum::<u32>())
+        .sorted_unstable()
+        .rev()
+        .take(3)
+        .collect::<Vec<u32>>();
+
+    println!("{}", calories[0]);
+    println!("{}", calories.into_iter().sum::<u32>())
+}
